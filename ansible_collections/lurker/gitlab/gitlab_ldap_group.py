ac3615178af590409ce4cc614120d4bad9050e21
MostTimeLurker <>
2026-03-01 08:00:52 +0100

    global permissions for groups, license adding

---
+++ ansible_collections/lurker/gitlab/gitlab_ldap_group.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Copyright: (c) 2026 MostTimeLurker
+# GNU General Public License v3.0+ (see LICENSES/GPL-3.0-or-later.txt or https://www.gnu.org/licenses/gpl-3.0.txt)
+# SPDX-License-Identifier: GPL-3.0-or-later
